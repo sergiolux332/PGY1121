bcdf766a10f977ccc49912d217ccb3268efc6b6c
sergiolux332 <134543116+sergiolux332@users.noreply.github.com>
2023-05-24 14:34:04 -0400

    código tipo py  sin validar

---
+++ clase-12/#ejemplo de uso de repositorio.py
@@ -0,0 +1,11 @@
+#ejemplo de uso de repositorio
+
+print("DATOS PERSONALES")
+print("------------------")
+vNom=input("ingrese su nombre")
+vEdad=input("ingrese su edad")
+print("-----------------------..")
+print(f"su nombre es: {vNom}")
+print(f"su edad es: {vEdad}")
+
+print("programa finalizado")
